41c9f6ae88828a1c1b8ad9d2bf295d5ba42e7fcc
Mattéo RENAUD <mrenaud2@cfai-formation.fr>
2025-10-30 15:50:48 +0100

    Test credentials

--- api/config.js
+++ api/config.js
@@ -1,11 +1,11 @@
 module.exports =  {
     ACCESS_TOKEN_SECRET : "EMMA123",
     BDD : {
-    "host" :"dpg-d40798bipnbc73cf4njg-a.oregon-postgres.render.com",
+    "host" :"dpg-d41mvbq4d50c73eto1ag-a",
     "port" : "5432",
-    "user" : "cnam_qnjc_user",
-    "password" : "FOYjmLxOmqwdnfGPhMO4R8eZpErPzVXQ",
-    "bdname" :"cnam_qnjc" 
+    "user" : "pollution_mzxu_user",
+    "password" : "3dIzkrm8lYw7YoPupodOFbfjus6lESrF",
+    "bdname" :"pollution_mzxu"
     }
 }
 
