806ccd149aec3d50eae9f0ee8d22c55cc94feb7e
Mattéo RENAUD <mrenaud2@cfai-formation.fr>
2025-10-31 14:32:40 +0100

    Test API avec route /get pollutions

---
+++ api/controllers/pollution.controllers.js
@@ -0,0 +1,16 @@
+const { v4: uuidv4 } = require ("uuid");
+
+const db = require("../models");
+const Pollution = db.pollution;
+const Op = db.Sequelize.Op;
+
+exports.get = (req, res) => {
+
+	Pollution.findAll()
+	.then(data => {res.send(data);})
+	.catch(err => {
+		res.status(400).send({
+			message: err.message
+		});
+	});
+};
